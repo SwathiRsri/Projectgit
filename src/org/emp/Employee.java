fa3189a5812a796b602cee1d415d8f8d2205a65e
SwathiRsri <swathimee29@gmail.com>
2023-02-01 20:52:22 +0530

    new code

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,19 @@
+package org.emp;
+import org.client.*; 
+import org.company.*;
+import org.project.*;
+public class Employee  {
+	private void empName() {
+		System.out.println("swathi");
+	}
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.empName();
+		Client cl = new Client();
+		cl.clientName();
+		Company co=new Company();
+		co.companyName();
+		Project pro = new Project();
+		pro.projectName();
+	}
+}
